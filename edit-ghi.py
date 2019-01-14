50d8a6bda9eeefd1a005dd125401ee016763b513
Peter Willis <peterwwillis@gmail.com>
2019-01-14 08:02:59 -0500

    Refactor a bit

--- edit-ghi.py
+++ edit-ghi.py
@@ -25,8 +25,12 @@ class Util(object):
         return subprocess.check_output(cmd)
 
 class GHIssues(Util):
-    issues = None
-    def load_gh_issues(self):
+    """ A class to manage GitHub issues """
+
+    def __init__(self):
+        self.cache = {}
+
+    def get_issues(self):
         """
         Get the list of GitHub Issues as a JSON dump.
         Data we get back that's useful:
@@ -40,8 +44,38 @@ class GHIssues(Util):
                 [ "ghi", "list", "--state", "all", "--json-out", "--quiet" ] 
         ).decode('utf-8')
         data = json.loads(issues)
-        #Debug("load_ghi(): \"%s\"" % data)
-        self.issues = data
+        Debug("load_ghi(): \"%s\"" % data)
+        return data
+
+    def issues(self):
+        if not self.cache:
+            self.cache = self.get_issues()
+        return self.cache
+
+    def issue(self, issue):
+        """ Look at the internal cache of GH issues and see if an issue matching
+            the dict 'issue' exists; if so, return True.
+
+            A GH issue is a Python datastructure of the JSON returned from the
+            GitHub Issues API. Common values include:
+             - url
+             - id
+             - node_id
+             - number
+             - title
+             - labels
+             - state
+        """
+        Debug("Checking if issue exists: '%s'" % issue)
+        for i in self.issues():
+            if 'title' in issue and 'title' in i and issue['title'] in i['title']:
+                Log("Found matching issue: issue[title] matches i[title]: '%s'" % i['title'])
+                return i
+        return None
+
+    def update_issue(self, issue, gh_issue):
+        Log("Updating GH issue '%s' with '%s'" % (gh_issue['number'], issue) )
+
 
 class EGFile(object):
     """
@@ -54,6 +88,7 @@ class EGFile(object):
         filename = path.name
         self.content = path.read()
         self._load_content()
+
     def _load_content(self):
         doci=0
         for line in self.content.splitlines():
@@ -63,10 +98,13 @@ class EGFile(object):
             if self.doc(doci) == None:
                 self.add_doc()
             self.doc(doci).add_line(line)
+
     def __repr__(self):
         return '<%s, docs: [%s]>' % (self.__class__.__name__, self.docs)
+
     def add_doc(self):
         self.docs.append( EGDoc() )
+
     def doc(self, num):
         if len(self.docs) >= (num+1):
             if self.docs[num] != None:
@@ -75,11 +113,14 @@ class EGFile(object):
 
 class EGDoc(object):
     """ A class to manage a list of issues """
+
     def __init__(self):
         self.heading = None
         self.issues = []
+
     def __repr__(self):
         return '<%s, issues: [%s]>' % (self.__class__.__name__, self.issues)
+
     def add_issue(self, args):
         if not 'title' in args:
             raise Exception("Error: all issues need a title (got: '%s')" % args)
@@ -129,44 +170,69 @@ class EGDoc(object):
             self.add_issue( args )
         return
 
-class EGIssue(object):
-    """ An object for an individual issue """
-    def __init__(self, args):
-        self.data = {}
-        for k in args:
-            self.data[k] = args[k]
-    def __repr__(self):
-        return '<%s, data: [%s]>' % (self.__class__.__name__, self.data)
+class EGIssue(dict):
+    """ A dict object for an individual issue.
+    
+        An EG Issue is the internal representation of issues that have been
+        read from a file. Common values include:
+         - heading (the '# some heading' part of a file)
+         - title (the title of the issue)
+         - state ('open' or 'closed')
+         - issues (a list of issue number references)
+         - tags (a list of tags)
+    """
+    def __init__(self, *arg, **kw):
+        super(EGIssue, self).__init__(*arg, **kw)
 
 
-
-class EditGhi(GHIssues):
+class EditGhi(object):
     """ Class to manage editing of GitHub issues """
 
-    def __init__(self, files):
+    def __init__(self, args):
         self.files = []
-        self.load_gh_issues()
-        if files != None:
-            for f in files:
+        self.gh = GHIssues()
+        self.args = args
+
+        if self.args.file != None:
+            for f in self.args.file:
                 self.load_file(f)
 
     def load_file(self, path):
         self.files.append( EGFile(path) )
 
+    def issues(self):
+        for egf in self.files:
+            for doc in egf.docs:
+                for issue in doc.issues:
+                    Debug("Issue: '%s'" % issue)
+                    yield issue
+        return
+
+    def fix_gh(self):
+        for issue in self.issues():
+            ghi = self.gh.issue(issue)
+            if ghi != None:
+                self.gh.update_issue(issue, ghi)
+
     def handle_issues(self):
         for egf in self.files:
             for doc in egf.docs:
                 for issue in doc.issues:
-                    print("Issue: '%s'" % issue)
+                    Debug("Issue: '%s'" % issue)
         return
 
     def edit_ghi(self):
-        self.handle_issues()
+        if self.args.fix_to_gh != None:
+            self.fix_gh()
 
 
 def options():
     parser = argparse.ArgumentParser(description='Edit GitHub issues in bulk')
     parser.add_argument('file', nargs=1, type=argparse.FileType('r'), help='The YAML file of issues to edit')
+    parser.add_argument('--add-to-gh', action='store_true', help='Add any issues found in the file to GitHub')
+    parser.add_argument('--add-to-file', action='store_true', help='Add any isses found in GitHub to the file')
+    parser.add_argument('--fix-to-gh', action='store_true', help='If there is inconsistency between the file and GitHub, apply the file issues to GitHub')
+    parser.add_argument('--fix-to-file', action='store_true', help='If there is inconsistency between the file and GitHub, apply GitHub issues to the file')
     return parser
 
 
@@ -174,7 +240,7 @@ def main():
     p = options()
     a = p.parse_args()
 
-    v = EditGhi( a.file )
+    v = EditGhi( a )
     v.edit_ghi()
 
 if __name__ == "__main__":
